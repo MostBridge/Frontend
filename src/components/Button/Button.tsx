79991079ac55b954b4c7025ebcf886cbd686ab65
Maria Lafutkina <lafutkina.masha@yandex.ru>
2023-10-24 20:25:21 +0300

    feature: to return Button.tsx

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,47 @@
+import { Button as MuiButton } from '@mui/material';
+import { ButtonProps } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const Button = styled(MuiButton)<ButtonProps>(({ theme }) => ({
+  elevation: 0,
+  textTransform: 'none',
+  variants: [
+    {
+      props: { size: 'medium' },
+      style: {
+        height: 50,
+        width: 250,
+        fontSize: 16,
+        lineHeight: 20,
+        fontWeight: 500,
+        padding: 15,
+      },
+    },
+    {
+      props: { size: 'small' },
+      style: {
+        height: 40,
+        width: 156,
+        fontSize: 14,
+        lineHeight: 20,
+        fontWeight: 400,
+        padding: '10px 20px',
+      },
+    },
+    {
+      props: { variant: 'outlined' },
+      style: {
+        borderColor: theme.palette.primary.main,
+        color: theme.palette.primary.contrastText,
+        fontWeght: 500,
+
+        '&:hover': {
+          backgroundColor: theme.palette.primary.main,
+          color: theme.palette.primary.contrastText,
+        },
+      },
+    },
+  ],
+}));
+
+export default Button;
