1551d2f1ed7d2241b6ad99447dbd40de1ce50ea9
a.voronova1 <a.voronova1@g.nsu.ru>
2023-10-28 02:48:18 +0700

    fix: fixed code style

--- src/components/AI/AI.tsx
+++ src/components/AI/AI.tsx
@@ -1,18 +1,20 @@
-import back from '../../assets/images/banner1.png'
+import back from '../../assets/images/banner1.png';
 import styles from './AI.module.css';
 import { Typography } from '@mui/material';
 import Button from 'components/Button/Button';
 
-function AI() {
+export default function AI() {
   return (
     <div className={styles.ai}>
-       <img src={back} alt='background' />
-       <div className={styles.ai__block}>
-       <Typography className={styles.title} variant="h2" component="h2" color="text.primary">Голосовой помощник по созданию вакансии</Typography>
-       <Button size='small' variant='outlined'>Попробовать</Button>
-       </div>
+      <img src={back} alt="background" />
+      <div className={styles.ai__block}>
+        <Typography className={styles.title} variant="h2" component="h2" color="text.primary">
+          Голосовой помощник по созданию вакансии
+        </Typography>
+        <Button size="small" variant="outlined">
+          Попробовать
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
-
-export default AI;
